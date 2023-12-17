97c1886eada5ac66c3596be7183d6ec2b630a0fa
Dan O'Meara <danom@google.com>
2023-12-17 15:38:11 -0800

    day5 ⭐⭐

--- 2023/day5.py
+++ 2023/day5.py
@@ -1,58 +1,67 @@
 import parse
 
+from typing import List
+
 from utilities import input
-
-
-class SourceRangeMap:
-    source_start = None
-    dest_start = None
-    span = None
-
-    def __init__(self, dest_start: int, source_start: int, span: int) -> None:
-        self.source_start = source_start
-        self.dest_start = dest_start
-        self.span = span
-
-    def range_contains(self, value: int) -> bool:
-        if value >= self.source_start and value < (self.source_start + self.span):
-            return True
-        return False
-
-    def get_destination(self, value: int) -> int:
-        if not self.range_contains(value):
-            raise ValueError
-        else:
-            return self.dest_start + (value - self.source_start)
-
-
-def next_destination(maps: list, map_index: int, value: int):
-    next = value
-    for r in maps[map_index]:
-        if r.range_contains(value):
-            next = r.get_destination(value)
-            break
-    return next
 
 
 def one(seeds: list, maps: list) -> int:
     locations = []
     for s in seeds:
         for m in range(0, len(maps)):
-            s = next_destination(maps, m, s)
+            for r in maps[m]:
+                if s >= r[1] and s < (r[1] + r[2]):
+                    s = (r[0] + s - r[1])
+                    break
         locations.append(s)
     return min(locations)
 
 
-def two(data: list) -> int:
-    return 0
+def transform(start: int, end: int, new_seeds: list[tuple[int]], seeds: list[tuple[int]], map: list[int]) -> int:
+    for dest, source, range_length in map:
+        # Check if the ranges overlap
+        overlap_start = max(start, source)
+        overlap_end = min(end, source + range_length)
+
+        if overlap_start < overlap_end:
+            new_seeds.append(
+                (
+                    dest + (overlap_start - source),
+                    dest + (overlap_end - source)
+                )
+            )
+
+            if start < overlap_start:
+                seeds.append((start, overlap_start))
+
+            if overlap_end < end:
+                seeds.append((overlap_end, end))
+
+            break
+    else:
+        # No overlap so add the original seed range to the "new seeds"
+        new_seeds.append((start, end))
+
+    return new_seeds, seeds
+
+
+def two(seeds: List[tuple[int]], maps: list) -> int:
+    for m in maps:
+        new_seeds = []
+        while len(seeds) > 0:
+            start, end = seeds.pop()
+            new_seeds, seeds = transform(start, end, new_seeds, seeds, m)
+        seeds = new_seeds
+    return min(seeds)[0]
 
 
 if __name__ == "__main__":
     data = input.str_array_from_list("5.txt")
     l1 = parse.parse("seeds: {}", data[0])
     seeds = [int(seed) for seed in l1[0].split()]
+    seed_ranges = [(seeds[i], seeds[i] + seeds[i + 1]) for i in range(0, len(seeds), 2)]
 
-    maps_list = [[] for _ in range(7)]
+    maps = [[] for _ in range(7)]
 
     maps_index = -1
     for line in data[1:]:
@@ -61,10 +70,11 @@ if __name__ == "__main__":
         p = parse.parse("{}-to-{} map:", line)
         if p:
             maps_index += 1
+            section = p[0]
             continue
-     
-        r = [int(x) for x in line.split()]
-        maps_list[maps_index].append(SourceRangeMap(r[0], r[1], r[2]))
 
-    print(f"Part 1: {one(seeds, maps_list)}")
-    print(f"Part 2: {two(data)}")
+        r = [int(x) for x in line.split()]
+        maps[maps_index].append([r[0], r[1], r[2]])
+
+    print(f"Part 1: {one(seeds, maps)}")
+    print(f"Part 2: {two(seed_ranges, maps)}")
