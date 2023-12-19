ba5b5cbe2877898253f41d2b2e95de11a6de7c96
Dan O'Meara <danom@google.com>
2023-12-18 16:38:57 -0800

    adds solution template

---
+++ 2023/day_.py
@@ -0,0 +1,20 @@
+from typing import List
+
+from utilities import input
+
+
+def one(data: List[str]) -> int:
+    
+    return 0
+
+
+def two(data: List[str]) -> int:
+    
+    return 0
+
+
+if __name__ == "__main__":
+    data = input.str_array_from_list("")
+
+    print(f"Part 1: {one(data)}")
+    # print(f"Part 2: {two(data)}")
