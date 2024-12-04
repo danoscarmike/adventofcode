1fcd0f5ddac107e3e5b8bce46bc90c5c86807790
Dan O'Meara <omeara.dan@gmail.com>
2024-12-04 12:52:20 -0800

    day 2 ⭐⭐

---
+++ 2024/day2.py
@@ -0,0 +1,63 @@
+from utilities.input import read_rows_from_file
+
+
+reports = read_rows_from_file("2.txt")
+
+
+def get_differences(row: list) -> list:
+    differences = []
+    for i in range(len(row) - 1):
+        differences.append(row[i + 1] - row[i])
+
+    return differences
+
+
+def report_is_safe(differences: list) -> bool:
+    if all([diff > 0 for diff in differences]) or all(
+        [diff < 0 for diff in differences]
+    ):
+        if all([abs(diff) < 4 for diff in differences]):
+            return True
+
+    return False
+
+
+def report_is_tolerable(row: list) -> bool:
+    differences = get_differences(row)
+    if report_is_safe(differences):
+        return True
+
+    for i in range(len(row)):
+        new_row = row[0:i] + row[i + 1 : len(row)]        
+        new_differences = get_differences(new_row)
+        if report_is_safe(new_differences):
+            return True
+
+    return False
+
+
+def part_one() -> int:
+    safe = 0
+
+    # create a list of the differences between list elements
+    for row in reports:
+        differences = get_differences(row)
+        if report_is_safe(differences):
+            safe += 1
+
+    return safe
+
+
+def part_two() -> int:
+    safe = 0
+
+    for row in reports:
+        if report_is_tolerable(row):
+            safe += 1
+
+    return safe
+
+
+if __name__ == "__main__":
+    print(f"Part One: {part_one()}")
+    print(f"Part Two: {part_two()}")
--- 2024/utilities/input.py
+++ 2024/utilities/input.py
@@ -1,12 +1,24 @@
 import os
 
-# read columns of integers from a file into multiple lists
+
+# read columns of integers from a file into a list of lists
 def read_columns_from_file(path: str, cols) -> list:
-    directory = '/2024/input/'
+    directory = "/2024/input/"
     columns = [[] for i in range(cols)]
     with open(os.getcwd() + directory + path, "r") as f:
         for line in f.readlines():
             for i in range(cols):
                 columns[i].append(int(line.split()[i]))
-        
+
     return columns
+
+
+# read rows of integers from a file into a list of lists
+def read_rows_from_file(path: str) -> list:
+    directory = "/2024/input/"
+    rows = []
+    with open(os.getcwd() + directory + path, "r") as f:
+        for line in f.readlines():
+            rows.append(list(map(int, line.split())))
+
+    return rows
