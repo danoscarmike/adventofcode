7f52d6c8ebe653155bd4531d579a7dee0479723e
Dan O'Meara <omeara.dan@gmail.com>
2024-12-10 22:05:59 -0800

    day 5 ⭐⭐

---
+++ 2024/day5.py
@@ -0,0 +1,89 @@
+import re
+
+from collections import defaultdict
+
+from utilities.input import read_rows_from_file
+
+
+def read_input(file_name: str) -> tuple:
+    rules = []
+    updates = []
+    input = read_rows_from_file(file_name, int=False)
+    for line in input:
+        pattern = r"(\d+)\|(\d+)"
+        match = re.search(pattern, line)
+        if match:
+            rules.append((int(match.group(1)), int(match.group(2))))
+        elif line == "":
+            pass
+        else:
+            updates.append([int(x) for x in line.split(",")])
+    
+    rule_dict = defaultdict(set)
+    for rule in rules:
+        rule_dict[rule[0]].add(rule[1])
+
+    return rule_dict, updates
+
+
+def is_valid_page_order(rule_dict: defaultdict, update: list) -> bool:
+    for i in range(len(update)):
+        for j in range(i + 1, len(update)):
+            if update[j] not in rule_dict[update[i]]:
+                return False
+
+    return True
+
+
+def triage_updates(rule_dict: defaultdict, updates: list) -> tuple:
+    good_updates = []
+    bad_updates = []
+
+    for update in updates:
+        if is_valid_page_order(rule_dict, update):
+            good_updates.append(update)
+        else:
+            bad_updates.append(update)
+
+    return good_updates, bad_updates
+
+
+def sort_update(rule_dict: defaultdict, update: list) -> list:
+    if is_valid_page_order(rule_dict, update):
+        return update
+    
+    sorted_update = update.copy()
+    for i in range(len(update)):
+        for j in range(i + 1, len(update)):
+            if update[j] not in rule_dict[update[i]]:
+                new_i = sorted_update.pop(j)
+                sorted_update.insert(i, new_i)
+                return sort_update(rule_dict, sorted_update)
+
+
+def part_one(good_updates) -> int:
+    total = 0
+    
+    for update in good_updates:
+        total += update[len(update) // 2]
+        
+    return total
+
+
+def part_two(sorted_updates) -> int:
+    total = 0 
+
+    sorted_updates = [sort_update(rules, update) for update in bad_updates]
+
+    for update in sorted_updates:
+        total += update[len(update) // 2]
+
+    return total
+
+
+if __name__ == "__main__":
+    rules, updates = read_input("5.txt")
+    good_updates, bad_updates = triage_updates(rules, updates)
+
+    print(f"Part One: {part_one(good_updates)}")
+    print(f"Part Two: {part_two(bad_updates)}")
--- 2024/test_all.py
+++ 2024/test_all.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day4 import part_one, part_two
+import day4, day5
 from utilities.input import read_rows_from_file
 
 
@@ -8,7 +8,19 @@ class TestDay4(unittest.TestCase):
     grid = read_rows_from_file('test_4.txt', int=False)
 
     def test_part_one(self):
-        assert part_one(self.grid, 'XMAS') == 18
+        assert day4.part_one(self.grid, 'XMAS') == 18
     
     def test_part_two(self):
-        assert part_two(self.grid) == 9
+        assert day4.part_two(self.grid) == 9
+
+
+class TestDay5(unittest.TestCase):
+    rules, updates = day5.read_input('test_5.txt')
+    good_updates, bad_updates = day5.triage_updates(rules, updates)
+
+    def test_part_one(self):
+        assert day5.part_one(self.good_updates) == 143
+    
+    def test_part_two(self):
+        sorted_updates = [day5.sort_update(self.rules, update) for update in self.bad_updates]
+        assert day5.part_two(sorted_updates) == 123
